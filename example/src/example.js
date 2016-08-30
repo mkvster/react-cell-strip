1f450aa94fa04d995e4050477202e264d61e1c9c
mkvster <mkvster@gmail.com>
2016-08-30 18:25:10 -0400

    Fixes #3

--- example/src/example.js
+++ example/src/example.js
@@ -5,10 +5,10 @@ var CellStrip = require('react-cell-strip');
 var App = React.createClass({
 	render () {
 		return (
-			<div style={{width: 422 + "px", height: 600 +"px"}}>
+			<div style={{width: 400 + "px", height: 600 +"px"}}>
 				<CellStrip cellWidth={100} cellHeight={25} cellNumbers={[61, 62, 63, 64]} />
 				<CellStrip cellWidth={[50, 120, 150, 80]}
-						cellHeight={[85.5, [30, 50], 85.5, [25,25,25]]}
+						cellHeight={[80, [30, 50], 80, [25,25,30]]}
 						cellNumbers={[1, [2, 3], 4, [5, 6, 7]]} />
 				<CellStrip cellWidth={[100, 50, 100, 150]} cellHeight={25}
 						cellNumbers={[8, 9, 10, 11]} />
