9c884aee0e8078c337183ba5efc87302d0fc5ddb
mkvster <mkvster@gmail.com>
2016-09-01 22:24:25 -0400

    Styling simplification

--- dist/react-cell-strip.js
+++ dist/react-cell-strip.js
@@ -7,77 +7,29 @@ Object.defineProperty(exports, "__esModule", {
 });
 var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
 
-var CellStripOLD = React.createClass({
-    displayName: "CellStripOLD",
-
-    getSize: function getSize(source, index, subIndex) {
-        if (source.constructor === Array) {
-            if (source[index].constructor === Array) {
-                return source[index][subIndex];
-            }
-            return source[index];
-        }
-        return source;
-    },
-    getWidth: function getWidth(index, subIndex) {
-        return this.getSize(this.props.cellWidth, index, subIndex);
-    },
-    getHeight: function getHeight(index, subIndex) {
-        return this.getSize(this.props.cellHeight, index, subIndex);
-    },
-    render: function render() {
-        var items = this.props.cellNumbers;
-        var innerArea = "";
-        var self = this;
-        var listItems = items.map(function (item, index) {
-            if (item.constructor === Array) {
-                var listSubItems = item.map(function (subitem, subIndex) {
-                    var w = self.getWidth(index, subIndex);
-                    var h = self.getHeight(index, subIndex);
-                    return React.createElement(
-                        "li",
-                        { key: subitem, className: "cell-board-noselect cell-board-col-li" },
-                        React.createElement(
-                            "div",
-                            { className: "cell-area", style: { width: w + "px", height: h + "px" } },
-                            subitem
-                        )
-                    );
-                });
-                innerArea = React.createElement(
-                    "div",
-                    null,
-                    React.createElement(
-                        "ul",
-                        { className: "cell-board-col-ul" },
-                        listSubItems
-                    )
-                );
-            } else {
-                var w = self.getWidth(index);
-                var h = self.getHeight(index);
-                innerArea = React.createElement(
-                    "div",
-                    { className: "cell-area", style: { width: w + "px", height: h + "px" } },
-                    item
-                );
-            }
+var PlainCell = function PlainCell(cellAreaClassName, cellNumberClassName) {
+    cellAreaClassName = cellAreaClassName || "cell-area";
+    cellNumberClassName = cellNumberClassName || "cell-number";
+    return React.createClass({
+        render: function render() {
             return React.createElement(
-                "li",
-                { key: item, className: "cell-board-noselect cell-board-row-li" },
-                innerArea
+                "div",
+                { className: cellAreaClassName,
+                    style: { width: this.props.width + "px",
+                        height: this.props.height + "px" } },
+                React.createElement(
+                    "div",
+                    { className: cellNumberClassName },
+                    this.props.cellNumber
+                )
             );
-        });
+        }
+    });
+};
 
-        return React.createElement(
-            "ul",
-            { className: "cell-board-row-ul" },
-            listItems
-        );
-    }
-});
+var CellStripTemplate = function CellStripTemplate(CellPresenter) {
+    CellPresenter = CellPresenter || PlainCell;
 
-var CellStrip = function CellStrip(CellPresenter) {
     return React.createClass({
 
         getSize: function getSize(source, index, subIndex) {
@@ -131,17 +83,21 @@ var CellStrip = function CellStrip(CellPresenter) {
                 );
             });
 
+            var rootClassName = this.props.className || "";
             return React.createElement(
                 "ul",
-                { className: "cell-board-row-ul" },
+                { className: rootClassName + " cell-board-row-ul" },
                 listItems
             );
         }
     });
 };
 
-exports["default"] = CellStrip;
-module.exports = exports["default"];
+var CellStrip = CellStripTemplate(PlainCell("", ""));
+
+exports.CellStripTemplate = CellStripTemplate;
+exports.PlainCell = PlainCell;
+exports.CellStrip = CellStrip;
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}]},{},[1])(1)
--- src/CellStrip.js
+++ src/CellStrip.js
@@ -1,72 +1,26 @@
 var React = require('react');
 
-var CellStripOLD = React.createClass({
-
-    getSize: function(source, index, subIndex) {
-        if (source.constructor === Array) {
-            if (source[index].constructor === Array) {
-                return source[index][subIndex];
-            }
-            return source[index];
-        }
-        return source;
-    },
-    getWidth: function(index, subIndex) {
-        return this.getSize(this.props.cellWidth, index, subIndex);
-    },
-    getHeight: function(index, subIndex) {
-        return this.getSize(this.props.cellHeight, index, subIndex);
-    },
-    render: function() {
-        var items = this.props.cellNumbers;
-				var innerArea = "";
-        var self = this;
-        var listItems = items.map(function(item, index) {
-          if (item.constructor === Array) {
-            var listSubItems = item.map(function(subitem, subIndex) {
-                var w = self.getWidth(index, subIndex);
-                var h = self.getHeight(index, subIndex);
-                return (
-                    <li key={subitem} className="cell-board-noselect cell-board-col-li">
-                          <div className="cell-area" style={{width: w + "px", height: h + "px"}}>
-                              {subitem}
-                          </div>
-                    </li>
-                );
-            });
-            innerArea = (
-                <div>
-                    <ul className="cell-board-col-ul">
-                      {listSubItems}
-                    </ul>
+var PlainCell = function(cellAreaClassName, cellNumberClassName) {
+    cellAreaClassName = cellAreaClassName || "cell-area";
+    cellNumberClassName = cellNumberClassName || "cell-number";
+    return React.createClass({
+        render: function() {
+            return (
+                <div className={cellAreaClassName}
+                    style={{width: this.props.width + "px",
+                      height: this.props.height + "px"}}>
+                    <div className={cellNumberClassName}>
+                      {this.props.cellNumber}
+                    </div>
                 </div>
             );
-          }
-          else {
-            var w = self.getWidth(index);
-            var h = self.getHeight(index);
-            innerArea = (
-                  <div className="cell-area" style={{width: w + "px", height: h + "px"}}>
-                      {item}
-                  </div>
-            );
-          }
-          return (
-            <li key={item} className="cell-board-noselect cell-board-row-li">
-                {innerArea}
-            </li>
-          );
-        });
+        }
+    });
+};
 
-        return (
-            <ul className="cell-board-row-ul">
-              {listItems}
-            </ul>
-        );
-    }
-});
+var CellStripTemplate = function(CellPresenter) {
+    CellPresenter = CellPresenter || PlainCell;
 
-var CellStrip = function(CellPresenter) {
     return React.createClass({
 
         getSize: function(source, index, subIndex) {
@@ -121,13 +75,18 @@ var CellStrip = function(CellPresenter) {
               );
             });
 
+            var rootClassName = this.props.className || "";
             return (
-                <ul className="cell-board-row-ul">
+                <ul className={rootClassName + " cell-board-row-ul"}>
                   {listItems}
                 </ul>
             );
         }
     });
-}
+};
 
-export default CellStrip;
+var CellStrip = CellStripTemplate(PlainCell("",""));
+
+export {CellStripTemplate}
+export {PlainCell};
+export {CellStrip};
